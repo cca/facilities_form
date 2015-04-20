8b6b5ea50620a4c452419ddb4cef2fbdce826311
Aaron Spafford <aaron.spafford@gmail.com>
2015-04-20 11:58:25 -0700

    toggle building options for sf/oakland

--- js/main.js
+++ js/main.js
@@ -3,15 +3,13 @@
   // This is the shortened form of $(document).ready().
   $(function() {
 
-    // campus building options
+    // toggle campus building options
   	$oakBuildingOptions = $('.form-component--oakland_building').hide();
   	$sfBuildingOptions = $('.form-component--san_francisco_building').hide();
 
   	$('.form-component--campus').change(function() {
 
 			var campusValue = $( ".form-component--campus input:radio:checked" ).val();
-
-      alert(campusValue);
 
 			if (campusValue == 'Oakland') {
 				$sfBuildingOptions.hide();
