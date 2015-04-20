eae6320c3c0688703405522c1c25d30a11cce252
Aaron Spafford <aaron.spafford@gmail.com>
2015-04-20 11:55:17 -0700

    toggle building options for sf/oakland

--- includes/helpers.php
+++ includes/helpers.php
@@ -9,12 +9,12 @@ function radio($name, $values) {
 		$options .= 
 	    '<div class="form-type-radio form-item radio">
 	    <input type="radio" id="' . $id . '" name="' . $id . '" value="' . $value . '" class="form-radio" />
-	    <label for="' . $id .'">' . $value . '"</label>
+	    <label for="' . $id .'">' . $value . '</label>
 	    </div>';
    }
 
   return 
-    '<div class="form-group">
+    '<div class="form-group form-component--' . $id . '">
 	  <label for="' . $id . '">' . $name . '</label>
 	  <div id="' . $name . '" class="form-radios">'
     . $options . '</div></div>';
@@ -25,7 +25,7 @@ function input($name, $type) {
 
 	$id = strtolower(str_replace(' ', '_', $name));
 
-  return '<div class="form-group">
+  return '<div class="form-group form-component--' . $id . '">
 				<label for="' . $id . '">' . $name . '</label>
 				<input type="' . $type  .'" class="form-control" id="' . $id . '" placeholder="' . $name . '">
 				</div>';
