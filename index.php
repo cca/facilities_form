3a94773b823c47bf04a6884c54892e9d875eca7a
Aaron Spafford <aaron.spafford@gmail.com>
2015-04-22 17:20:16 -0700

    refactor

--- index.php
+++ index.php
@@ -14,7 +14,30 @@
       <div class="row">
           <div class="region region-content col-sm-8">
 
-				<?php include_once "template/body.php"; ?>
+          <h1>Facilities Service Request Form</h1>
+
+          <?php
+          include_once "template/fields.php";
+
+          // validation
+          $success = FALSE;
+          if ($_POST["op"] == "Submit") {
+            if($errors = validate($_POST, $fields)) {
+              print $errors;
+            } else {
+              $success = TRUE;
+            }
+          }
+
+          if ($success) {
+            include_once "template/success.php";
+            // send email
+            email_message($_POST, $fields);
+
+          } else {
+            include_once "template/form.php";
+          }
+          ?>
 
         </div><!-- /region-content -->
 		  </div><!-- /row -->
