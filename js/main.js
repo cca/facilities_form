429592bddc10b6bc9e77f9657dc6e4226115ed2f
Aaron Spafford <aaron.spafford@gmail.com>
2015-04-24 17:15:25 -0700

    fix campus building option toggle

--- js/main.js
+++ js/main.js
@@ -3,29 +3,22 @@
   // This is the shortened form of $(document).ready().
   $(function() {
 
-    // toggle campus building options
+    // hide campus building options
   	$oakBuildingOptions = $('.form-component--oak_building').hide();
   	$sfBuildingOptions = $('.form-component--sf_building').hide();
-
-    var campusValue = $( ".form-component--campus input:radio:checked" ).val();
-
-    if (campusValue == 'Oakland') {
-      $sfBuildingOptions.hide();
-      $oakBuildingOptions.show();
-    } 
-    if (campusValue == "San Francisco") {
-      $oakBuildingOptions.hide();
-      $sfBuildingOptions.show();
-    }
 
   	$('.form-component--campus').change(function() {
 
 			campusValue = $( ".form-component--campus input:radio:checked" ).val();
 
+      // toggle campus building options
 			if (campusValue == 'Oakland') {
+        $oakBuildingOptions = $('.form-component--oak_building').hide();
+        $('.form-component--sf_building input:radio:checked').removeAttr('checked');
 				$sfBuildingOptions.hide();
 				$oakBuildingOptions.show();
 			} else {
+        $('.form-component--oak_building input:radio:checked').removeAttr('checked');
 				$oakBuildingOptions.hide();
 				$sfBuildingOptions.show();
 			}
