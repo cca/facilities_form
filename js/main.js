877ac2d40c8a2a5a1d840c7b1f18ae5fbaaa8d46
Mark Y. Goh <mgoh@cca.edu>
2015-09-24 13:56:38 -0700

    added reshall

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@
     if (campusValue != 'San Francisco') {
     	$('.form-component--sf_building').hide();
     }
+    if (campusValue != 'Residence Halls') {
+    	$('.form-component--reshall_building').hide();
+    }
 
   	$('.form-component--campus').change(function() {
 
@@ -19,11 +22,22 @@
       // toggle campus building options
 			if (campusValue == 'Oakland') {
         $('.form-component--sf_building input:radio:checked').removeAttr('checked');
+        $('.form-component--reshall_building input:radio:checked').removeAttr('checked');
+        $('.form-component--reshall_building').hide();
         $('.form-component--sf_building').hide();
         $('.form-component--oak_building').show();
+			} else if (campusValue == "Residence Halls") {
+        $('.form-component--sf_building input:radio:checked').removeAttr('checked');
+        $('.form-component--oak_building input:radio:checked').removeAttr('checked');
+        $('.form-component--reshall_building').show();
+        $('.form-component--sf_building').hide();
+        $('.form-component--oak_building').hide();
+			}
 			} else {
         $('.form-component--oak_building input:radio:checked').removeAttr('checked');
+        $('.form-component--reshall_building input:radio:checked').removeAttr('checked');
         $('.form-component--sf_building').show();
+        $('.form-component--reshall_building').hide();
         $('.form-component--oak_building').hide();
 			}
   	});
