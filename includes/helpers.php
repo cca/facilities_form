e71718605ae62a848be6c6828ef0872b8ababbdf
Mark Y. Goh <mgoh@cca.edu>
2015-09-24 17:30:21 -0700

    fixed references to campus now named location

--- includes/helpers.php
+++ includes/helpers.php
@@ -146,10 +146,10 @@ function email_message($form, $fields) {
   // compus abreviation
   $campus_short = array("San Francisco"=>"SF", "Oakland"=>"OAK", "Housing"=>"Housing");
   // get oak or sf building
-  $building = strtolower($campus_short[$form['campus']]) . '_building';
+  $building = strtolower($campus_short[$form['location']]) . '_building';
 
-	$to = $campus_email[$form['campus']];
-	$subject = $form['title'] . ' ' . $campus_short[$form['campus']] . '- ' . $form[$building] . ' ' . $form['area'];
+	$to = $campus_email[$form['location']];
+	$subject = $form['title'] . ' ' . $campus_short[$form['location']] . '- ' . $form[$building] . ' ' . $form['area'];
   $headers = "From: " . strip_tags($form['email']) . "\r\n";
   $headers .= "Reply-To: ". strip_tags($form['email']) . "\r\n";
   $headers .= "MIME-Version: 1.0\r\n";
