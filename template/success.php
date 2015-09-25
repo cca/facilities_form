5d64e13b05f37b22d236c8ee8906a2fa7a0cd07d
Mark Y. Goh <mgoh@cca.edu>
2015-09-24 17:39:27 -0700

    changed wording so its not so damn weird with Residence halls

--- template/success.php
+++ template/success.php
@@ -1,3 +1,3 @@
-<p>Thank you! Your request has been submitted to the CCA Facilities department in <?php print $_POST['location']; ?>!</p>
+<p>Thank you! Your request has been submitted to the CCA Facilities department in charge of <?php print $_POST['location']; ?>!</p>
 
 <p>You will receive an email confirmation at <strong><?php print $_POST['email']; ?></strong> once your request has been received.</p>
